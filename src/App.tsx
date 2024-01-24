4772fc6273eef1688992944a8f0ff5b338a3d6e7
Azeem46 <shaikhazeem4646@gmail.com>
2024-01-24 23:03:55 +0530

    removed unnessary banner from top

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Authenticated, GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { Authenticated, Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
@@ -25,7 +25,6 @@ import EditTask from "./pages/tasks/edit";
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
           <AntdApp>
             <DevtoolsProvider>
